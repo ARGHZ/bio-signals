270db08c1d731813348404b30e96599adf12dc4c
Juan Carrillo <juan.carrillolpz@uanl.edu.mx>
2019-11-05 17:54:37 -0600

    feat: define makecoefcorrthroughconvolution and getzerofilledsecondvector
    Two plots are saves concerning coefcorr vectors

--- notebooks/second_project.py
+++ notebooks/second_project.py
@@ -22,6 +22,19 @@ def displaycorrelationgraphs(first_vector, second_vector):
     fig.show()
 
 
+def getzerofilledsecondvector(vector_a, vector_b, mode='after'):
+    shape_diff = abs(vector_a.shape[0] - vector_b.shape[0])
+    relocated_subset = vector_b
+    if shape_diff > 0:
+        filling_arr = np.zeros(shape_diff)
+        if mode == 'after':
+            relocated_subset = np.append(vector_b, filling_arr)
+        else:
+            relocated_subset = np.insert(vector_b, 0, filling_arr)
+
+    return relocated_subset
+
+
 def searchformaxcoefcorr(template, (start, finish), signal):
     subset = signal[start:finish]
     template_max_value_idx = np.argmax(template)
@@ -41,7 +54,7 @@ def searchformaxcoefcorr(template, (start, finish), signal):
     merged_vectors = np.array([template, relocated_subset])
     merged_vectors = np.mean(merged_vectors, axis=0)
 
-    plt.plot(template, 'k', relocated_subset, 'c', merged_vectors, 'r--')
+    plt.plot(template, 'k', relocated_subset, 'c', merged_vectors, 'r--', subset, 'g+')
     title = 'PQRST with CoefCorr {}'.format(coef_corr)
     plt.title(title)
     plt.margins(0, 0.1)
@@ -82,6 +95,58 @@ def selectqrsfromsignal(data):
     #ax_orig.margins(0, 0.1)
     #fig.tight_layout()
     fig.show()
+
+
+def makecoefcorrthroughconvolution(data):
+    signal_length = len(data)
+    init_idx, end_idx = 0, 700
+    subset_template = data[init_idx:end_idx]
+    step_length = len(subset_template)
+
+    corrcoefs, data_iter = [], range(len(data))
+    for i in data_iter:
+        start, finish = i - step_length, i
+
+        if start < 0:
+            subset_data = data[:finish + 1]
+            subset_data_length = len(subset_data)
+            #print "ith: {} | ({} - {})".format(i, start, finish)
+            diff = abs(subset_data_length - step_length)
+            subset_data = getzerofilledsecondvector(subset_template, subset_data, mode='before')
+            #print 'negative indexes | {}'.format(diff)
+            #print 'subset length: {} before | subset length: {} after'.format(subset_data_length, len(subset_data))
+            corr_coef = np.corrcoef(subset_template, subset_data)
+            corr_coef = round(corr_coef[0][1], 4)
+        else:
+            subset_data = data[start:finish]
+            subset_data_length = len(subset_data)
+            corr_coef = np.corrcoef(subset_template, subset_data)
+            corr_coef = round(corr_coef[0][1], 4)
+
+        corrcoefs.append(corr_coef)
+        #print "----------------------------------------------------------------------------------------------------"
+
+    print 'First iterator has finished'
+    threshold_95 = np.repeat(0.95, len(corrcoefs))
+    threshold_40 = np.repeat(0.40, len(corrcoefs))
+
+    plt.plot(corrcoefs, 'g', threshold_95, 'c--', threshold_40, 'm-.')
+    plt.show()
+    plt.clf()
+
+    averaged_signal = []
+    step_length = abs(init_idx - end_idx)
+    step_iter = np.arange(0, signal_length, step_length)
+    step_iter = step_iter[1:]
+    for idx in step_iter:
+        start, finish = idx - step_length, idx
+        #print '[{}, {}]'.format(start, finish)
+        corr_coef = searchformaxcoefcorr(subset_template, (start, finish), data)
+        averaged_PQRST = corr_coef['vector']
+        averaged_signal.extend(averaged_PQRST)
+        #print "({} - {}) === coef corr: {}".format(start, finish, coef_corr['coef_corr'])
+
+    corr = spsignal.correlate(data, averaged_signal, 'same')
 
 
 def gridsearchbutterworthfilter(dataset, scattered_params, btype='low'):
